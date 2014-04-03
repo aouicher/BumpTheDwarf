a08ddc0b68561c285a080c8b882a4d5ee0a8e1fa
Alexandre Ouicher <aouicher@cobalt.fr>
2014-04-03 12:16:53 +0200

    Add levelScene

---
+++ BumpTheDwarf/Classes/LevelScene.h
@@ -0,0 +1,13 @@
+//
+//  LevelScene.h
+//  BumpTheDwarf
+//
+//  Created by Alexandre Ouicher on 03/04/2014.
+//  Copyright (c) 2014 Cobalt. All rights reserved.
+//
+
+#import "CCScene.h"
+
+@interface LevelScene : CCScene
+
+@end
---
+++ BumpTheDwarf/Classes/LevelScene.m
@@ -0,0 +1,13 @@
+//
+//  LevelScene.m
+//  BumpTheDwarf
+//
+//  Created by Alexandre Ouicher on 03/04/2014.
+//  Copyright (c) 2014 Cobalt. All rights reserved.
+//
+
+#import "LevelScene.h"
+
+@implementation LevelScene
+
+@end
