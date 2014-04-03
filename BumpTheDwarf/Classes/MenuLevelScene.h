b7d2cc019056f8f8c73f1d139689b08da0ded6dc
Alexandre Ouicher <aouicher@cobalt.fr>
2014-04-03 12:26:08 +0200

    Add MenuLevelScene

---
+++ BumpTheDwarf/Classes/MenuLevelScene.h
@@ -0,0 +1,13 @@
+//
+//  MenuLevelScene.h
+//  BumpTheDwarf
+//
+//  Created by Alexandre Ouicher on 03/04/2014.
+//  Copyright (c) 2014 Cobalt. All rights reserved.
+//
+
+#import "CCScene.h"
+
+@interface MenuLevelScene : CCScene
+
+@end
---
+++ BumpTheDwarf/Classes/MenuLevelScene.m
@@ -0,0 +1,13 @@
+//
+//  MenuLevelScene.m
+//  BumpTheDwarf
+//
+//  Created by Alexandre Ouicher on 03/04/2014.
+//  Copyright (c) 2014 Cobalt. All rights reserved.
+//
+
+#import "MenuLevelScene.h"
+
+@implementation MenuLevelScene
+
+@end
