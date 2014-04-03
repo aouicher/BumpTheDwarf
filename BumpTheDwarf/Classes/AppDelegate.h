004c267a7b9b994fc2bd62499ac9cec272a2ddb2
Alexandre Ouicher <aouicher@cobalt.fr>
2014-04-03 12:14:54 +0200

    Initial Commit

---
+++ BumpTheDwarf/Classes/AppDelegate.h
@@ -0,0 +1,13 @@
+//
+//  AppDelegate.h
+//  BumpTheDwarf
+//
+//  Created by Alexandre Ouicher on 03/04/2014.
+//  Copyright Cobalt 2014. All rights reserved.
+//
+// -----------------------------------------------------------------------
+
+#import "cocos2d.h"
+
+@interface AppDelegate : CCAppDelegate
+@end
---
+++ BumpTheDwarf/Supporting Files/main.m
@@ -0,0 +1,17 @@
+//
+//  main.m
+//  BumpTheDwarf
+//
+//  Created by Alexandre Ouicher on 03/04/2014.
+//  Copyright Cobalt 2014. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+int main(int argc, char *argv[]) {
+    
+    @autoreleasepool {
+        int retVal = UIApplicationMain(argc, argv, nil, @"AppDelegate");
+        return retVal;
+    }
+}
